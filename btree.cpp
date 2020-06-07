ebfbae55e5cf207c5f3d1f47b98f867d304cc092
Irena Niu <irenaniu@gmail.com>
2020-06-07 01:11:55 -0700

    edit test

--- btree.cpp
+++ btree.cpp
@@ -14,37 +14,43 @@ BTreeIndex::BTreeIndex(DbRelation &relation, Identifier name, ColumnNames key_co
                                                                                                       root(nullptr),
                                                                                                       file(relation.get_table_name() +
                                                                                                            "-" + name),
-                                                                                                      key_profile() {
+                                                                                                      key_profile()
+{
     if (!unique)
         throw DbRelationError("BTree index must have unique key");
     build_key_profile();
 }
 
-BTreeIndex::~BTreeIndex() {
+BTreeIndex::~BTreeIndex()
+{
     delete stat;
     delete root;
 }
 
 // Create the index.
-void BTreeIndex::create() {
+void BTreeIndex::create()
+{
     file.create();
     stat = new BTreeStat(file, STAT, STAT + 1, key_profile);
     root = new BTreeLeaf(file, stat->get_root_id(), key_profile, true);
     closed = false;
     Handles *table_rows = relation.select();
-    for (auto const &row: *table_rows)
+    for (auto const &row : *table_rows)
         insert(row);
     delete table_rows;
 }
 
 // Drop the index.
-void BTreeIndex::drop() {
+void BTreeIndex::drop()
+{
     file.drop();
 }
 
 // Open existing index. Enables: lookup, range, insert, delete, update.
-void BTreeIndex::open() {
-    if (closed) {
+void BTreeIndex::open()
+{
+    if (closed)
+    {
         file.open();
         stat = new BTreeStat(file, STAT, key_profile);
         if (stat->get_height() == 1)
@@ -56,8 +62,10 @@ void BTreeIndex::open() {
 }
 
 // Closes the index. Disables: lookup, range, insert, delete, update.
-void BTreeIndex::close() {
-    if (!closed) {
+void BTreeIndex::close()
+{
+    if (!closed)
+    {
         file.close();
         delete stat;
         stat = nullptr;
@@ -69,42 +77,50 @@ void BTreeIndex::close() {
 
 // Find all the rows whose columns are equal to key. Assumes key is a dictionary whose keys are the column
 // names in the index. Returns a list of row handles.
-Handles *BTreeIndex::lookup(ValueDict *key_dict) const {
+Handles *BTreeIndex::lookup(ValueDict *key_dict) const
+{
     return this->_lookup(this->root, this->stat->get_height(), this->tkey(key_dict));
 }
 
-Handles *BTreeIndex::range(ValueDict *min_key, ValueDict *max_key) const {
+Handles *BTreeIndex::range(ValueDict *min_key, ValueDict *max_key) const
+{
     throw DbRelationError("Don't know how to do a range query on Btree index yet");
     // FIXME
 }
 
 // Helper function for lookup
-Handles* BTreeIndex::_lookup(BTreeNode *node, uint height, const KeyValue* key) const {
-    Handles* handles = new Handles;
-    BTreeLeaf* leaf = nullptr;
-    if(height == 1) {
-        leaf = (BTreeLeaf*)node;
-        try { 
-            handles->push_back(leaf->find_eq(key)); 
+Handles *BTreeIndex::_lookup(BTreeNode *node, uint height, const KeyValue *key) const
+{
+    Handles *handles = new Handles;
+    BTreeLeaf *leaf = nullptr;
+    if (height == 1)
+    {
+        leaf = (BTreeLeaf *)node;
+        try
+        {
+            handles->push_back(leaf->find_eq(key));
         }
-        catch (...) { 
-            return handles; 
+        catch (...)
+        {
+            return handles;
         }
         return handles;
-
     }
-    else {
-        return this->_lookup(((BTreeInterior*)node)->find(key, height), height - 1, key);
+    else
+    {
+        return this->_lookup(((BTreeInterior *)node)->find(key, height), height - 1, key);
     }
 }
 
 // Insert a row with the given handle. Row must exist in relation already.
-void BTreeIndex::insert(Handle handle) {
+void BTreeIndex::insert(Handle handle)
+{
     open();
     ValueDict *key = relation.project(handle);
     KeyValue *tkey = this->tkey(key);
     Insertion insertion = _insert(root, stat->get_height(), tkey, handle);
-    if (!BTreeNode::insertion_is_none(insertion)) {
+    if (!BTreeNode::insertion_is_none(insertion))
+    {
         auto *new_root = new BTreeInterior(file, 0, key_profile, true);
         new_root->set_first(root->get_id());
         new_root->insert(&insertion.second, insertion.first);
@@ -121,11 +137,15 @@ void BTreeIndex::insert(Handle handle) {
 }
 
 // Recursive insert. If a split happens at this level, return the (new node, boundary) of the split.
-Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue *key, Handle handle) {
-    if (height == 1) {
+Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue *key, Handle handle)
+{
+    if (height == 1)
+    {
         auto *leaf = dynamic_cast<BTreeLeaf *>(node);
         return leaf->insert(key, handle);
-    } else {
+    }
+    else
+    {
         auto *interior = dynamic_cast<BTreeInterior *>(node);
         Insertion insertion = _insert(interior->find(key, height), height - 1, key, handle);
         if (!BTreeNode::insertion_is_none(insertion))
@@ -134,32 +154,37 @@ Insertion BTreeIndex::_insert(BTreeNode *node, uint height, const KeyValue *key,
     }
 }
 
-void BTreeIndex::del(Handle handle) {
+void BTreeIndex::del(Handle handle)
+{
     throw DbRelationError("Don't know how to delete from a BTree index yet");
     // FIXME
 }
 
-KeyValue *BTreeIndex::tkey(const ValueDict *key) const {
+KeyValue *BTreeIndex::tkey(const ValueDict *key) const
+{
     KeyValue *key_value = new KeyValue();
-    for (auto const &column_name: key_columns)
+    for (auto const &column_name : key_columns)
         key_value->push_back(key->find(column_name)->second);
     return key_value;
 }
 
 // Figure out the data types of each key component and encode them in key_profile, a list of int/str classes.
-void BTreeIndex::build_key_profile() {
+void BTreeIndex::build_key_profile()
+{
     std::map<const Identifier, ColumnAttribute::DataType> types_by_colname;
     const ColumnAttributes column_attributes = relation.get_column_attributes();
     uint col_num = 0;
-    for (auto const &column_name: relation.get_column_names()) {
+    for (auto const &column_name : relation.get_column_names())
+    {
         ColumnAttribute ca = column_attributes[col_num++];
         types_by_colname[column_name] = ca.get_data_type();
     }
-    for (auto const &column_name: key_columns)
+    for (auto const &column_name : key_columns)
         key_profile.push_back(types_by_colname[column_name]);
 }
 
-bool test_btree() {
+bool test_btree()
+{
     ColumnNames column_names;
     column_names.push_back("a");
     column_names.push_back("b");
@@ -175,7 +200,8 @@ bool test_btree() {
     row2["b"] = Value(101);
     table.insert(&row1);
     table.insert(&row2);
-    for (int i = 0; i < 100 * 1000; i++) {
+    for (int i = 0; i < 100 * 1000; i++)
+    {
         ValueDict row;
         row["a"] = Value(i + 100);
         row["b"] = Value(-i);
@@ -185,14 +211,13 @@ bool test_btree() {
     column_names.push_back("a");
     BTreeIndex index(table, "fooindex", column_names, true);
     index.create();
-    //return true;  // FIXME
-
 
     ValueDict lookup;
     lookup["a"] = 12;
     Handles *handles = index.lookup(&lookup);
     ValueDict *result = table.project(handles->back());
-    if (*result != row1) {
+    if (*result != row1)
+    {
         std::cout << "first lookup failed" << std::endl;
         return false;
     }
@@ -201,7 +226,8 @@ bool test_btree() {
     lookup["a"] = 88;
     handles = index.lookup(&lookup);
     result = table.project(handles->back());
-    if (*result != row2) {
+    if (*result != row2)
+    {
         std::cout << "second lookup failed" << std::endl;
         return false;
     }
@@ -209,12 +235,13 @@ bool test_btree() {
     delete result;
     lookup["a"] = 6;
     handles = index.lookup(&lookup);
-    if (handles->size() != 0) {
+    if (handles->size() != 0)
+    {
         std::cout << "third lookup failed" << std::endl;
         return false;
     }
     delete handles;
-
+    /**
     for (uint j = 0; j < 10; j++)
         for (int i = 0; i < 1000; i++) {
             lookup["a"] = i + 100;
@@ -229,7 +256,24 @@ bool test_btree() {
             delete handles;
             delete result;
         }
-    return true;  // FIXME
+**/
+    for (uint j = 0; j < 2; j++)
+        for (int i = 0; i < 39; i++)
+        {
+            lookup["a"] = i + 100;
+            handles = index.lookup(&lookup);
+            result = table.project(handles->back());
+            row1["a"] = i + 100;
+            row1["b"] = -i;
+            if (*result != row1)
+            {
+                std::cout << "lookup failed " << i << std::endl;
+                return false;
+            }
+            delete handles;
+            delete result;
+        }
+    return true; // Only test for lookup
 
     // test delete
     ValueDict row;
@@ -242,7 +286,8 @@ bool test_btree() {
     thandle = handles->back();
     delete handles;
     result = table.project(thandle);
-    if (*result != row) {
+    if (*result != row)
+    {
         std::cout << "44 lookup failed" << std::endl;
         return false;
     }
@@ -250,7 +295,8 @@ bool test_btree() {
     index.del(thandle);
     table.del(thandle);
     handles = index.lookup(&lookup);
-    if (handles->size() != 0) {
+    if (handles->size() != 0)
+    {
         std::cout << "delete failed" << std::endl;
         return false;
     }
@@ -262,8 +308,10 @@ bool test_btree() {
     maxkey["a"] = 310;
     handles = index.range(&minkey, &maxkey);
     ValueDicts *results = table.project(handles);
-    for (int i = 0; i < 210; i++) {
-        if (results->at(i)->at("a") != Value(100 + i)) {
+    for (int i = 0; i < 210; i++)
+    {
+        if (results->at(i)->at("a") != Value(100 + i))
+        {
             ValueDict *wrong = results->at(i);
             std::cout << "range failed: " << i << ", a: " << wrong->at("a").n << ", b: " << wrong->at("b").n
                       << std::endl;
@@ -271,7 +319,7 @@ bool test_btree() {
         }
     }
     delete handles;
-    for (auto vd: *results)
+    for (auto vd : *results)
         delete vd;
     delete results;
 
@@ -281,7 +329,8 @@ bool test_btree() {
     delete handles;
     handles = table.select();
     u_long count_t = handles->size();
-    if (count_i != count_t) {
+    if (count_i != count_t)
+    {
         std::cout << "full range failed: " << count_i << std::endl;
         return false;
     }
@@ -291,7 +340,8 @@ bool test_btree() {
     handles = index.range(nullptr, nullptr);
     count_i = handles->size();
     delete handles;
-    if (count_i != 0) {
+    if (count_i != 0)
+    {
         std::cout << "delete everything failed: " << count_i << std::endl;
         return false;
     }
@@ -299,4 +349,3 @@ bool test_btree() {
     table.drop();
     return true;
 }
-
